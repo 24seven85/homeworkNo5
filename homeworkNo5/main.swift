3797b8cdd9c1f0b8492453f264ab56266938ee59
maksimkjaulakis <24seven@live.ru>
2020-12-27 16:16:57 +0200

    add extension and methods

--- homeworkNo5/main.swift
+++ homeworkNo5/main.swift
@@ -10,11 +10,41 @@ import Foundation
 enum Transmission {
     case automatic, manual
 }
+enum Windows {
+    case open, close
+}
+enum Engine {
+    case start, stop
+}
+enum Wheels {
+    case summer, winter
+}
+
 protocol Car {
     var brand: String { get  }
-    var transmission: Transmission { get set }
-    var wheels: Int { get set }
+    var transmission: Transmission { get  }
+    var wheels: Wheels { get set }
     var km: Double { get set }
+    var windows: Windows { get set }
+    var engine: Engine { get set }
     
     func printCar()
 }
+extension Car {
+    
+    mutating func openCloseWindow(window: Windows) {
+        self.windows = window
+    }
+    mutating func startStopEngine(engine: Engine) {
+        self.engine = engine
+    }
+
+}
+
+extension Car {
+    
+    mutating func changewheels(wheel: Wheels) {
+        self.wheels = wheel
+    }
+}
+
